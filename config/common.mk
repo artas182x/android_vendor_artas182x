bec149d99f192216708ad46d9e652a4817c1c9a9
artas182x <artas182x@gmail.com>
2014-12-01 21:35:39 +0100

    Update vendor name

--- config/common.mk
+++ config/common.mk
@@ -1,8 +1,8 @@
-## Common FML Config ##
+## Common artas182x Config ##
 
 # Superuser
 #SUPERUSER_EMBEDDED := true
-#SUPERUSER_PACKAGE_PREFIX := com.android.settings.fml.superuser
+#SUPERUSER_PACKAGE_PREFIX := com.android.settings.artas182x.superuser
 
 #PRODUCT_PACKAGES += \
 #	Superuser \
@@ -13,11 +13,11 @@
 
 # Boot animation
 PRODUCT_COPY_FILES += \
-	vendor/fml/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+	vendor/artas182x/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
-# FML init.rc
+# artas182x init.rc
 PRODUCT_COPY_FILES += \
-	vendor/fml/prebuilt/common/etc/init.fml.rc:root/init.fml.rc
+	vendor/artas182x/prebuilt/common/etc/init.artas182x.rc:root/init.artas182x.rc
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -36,24 +36,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # init.d support
 PRODUCT_COPY_FILES += \
-	vendor/fml/prebuilt/common/bin/sysinit:system/bin/sysinit
+	vendor/artas182x/prebuilt/common/bin/sysinit:system/bin/sysinit \
+	vendor/artas182x/prebuilt/common/etc/init.d/00root:system/etc/init.d/00root
 
 #root
 #PRODUCT_COPY_FILES += \
-#    vendor/fml/prebuilt/common/bin/app_process:system/bin/app_process \
-#    vendor/fml/prebuilt/common/bin/app_process32:system/bin/app_process32 \
-#    vendor/fml/prebuilt/common/bin/app_process32_original:system/bin/app_process32_original \
-#    vendor/fml/prebuilt/common/bin/app_process_init:system/bin/app_process_init \
-#    vendor/fml/prebuilt/common/bin/install-recovery.sh:system/bin/install-recovery.sh \
-#    vendor/fml/prebuilt/common/bin/.ext/.su:system/bin/.ext/.su \
-#    vendor/fml/prebuilt/common/app/SuperSU/SuperSU.apk:system/app/SuperSU/SuperSU.apk \
-#    vendor/fml/prebuilt/common/etc/init.d/00root:system/etc/init.d/00root \
-#    vendor/fml/prebuilt/common/lib/libsupol.so:system/lib/libsupol.so \
-#    vendor/fml/prebuilt/common/xbin/daemonsu:system/xbin/daemonsu \
-#    vendor/fml/prebuilt/common/xbin/su:system/xbin/su \
-#    vendor/fml/prebuilt/common/xbin/sugote:system/xbin/sugote \
-#    vendor/fml/prebuilt/common/xbin/sugote-mksh:system/xbin/sugote-mksh \
-#    vendor/fml/prebuilt/common/xbin/supolicy:system/xbin/supolicy    
+#    vendor/artas182x/prebuilt/common/bin/app_process:system/bin/app_process \
+#    vendor/artas182x/prebuilt/common/bin/app_process32:system/bin/app_process32 \
+#    vendor/artas182x/prebuilt/common/bin/app_process32_original:system/bin/app_process32_original \
+#    vendor/artas182x/prebuilt/common/bin/app_process_init:system/bin/app_process_init \
+#    vendor/artas182x/prebuilt/common/bin/install-recovery.sh:system/bin/install-recovery.sh \
+#    vendor/artas182x/prebuilt/common/bin/.ext/.su:system/bin/.ext/.su \
+#    vendor/artas182x/prebuilt/common/app/SuperSU/SuperSU.apk:system/app/SuperSU/SuperSU.apk \
+#    vendor/artas182x/prebuilt/common/etc/init.d/00root:system/etc/init.d/00root \
+#    vendor/artas182x/prebuilt/common/lib/libsupol.so:system/lib/libsupol.so \
+#    vendor/artas182x/prebuilt/common/xbin/daemonsu:system/xbin/daemonsu \
+#    vendor/artas182x/prebuilt/common/xbin/su:system/xbin/su \
+#    vendor/artas182x/prebuilt/common/xbin/sugote:system/xbin/sugote \
+#    vendor/artas182x/prebuilt/common/xbin/sugote-mksh:system/xbin/sugote-mksh \
+#    vendor/artas182x/prebuilt/common/xbin/supolicy:system/xbin/supolicy    
 
 PRODUCT_PACKAGES += \
 	CellBroadcastReceiver \
